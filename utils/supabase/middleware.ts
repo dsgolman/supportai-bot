9f6450a5cd7b793d5561f66c7c4d7b82eabda7c4
Peter Jones <pjxcvi@proton.me>
2024-09-05 16:47:17 -0400

    fix middleware function in supabase

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -1,20 +1,36 @@
-// @/utils/supabase/middleware.ts
-import { NextRequest, NextResponse } from 'next/server'
-import { createClient } from '@/utils/supabase/client'
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { NextResponse, type NextRequest } from "next/server";
 
 export async function updateSession(request: NextRequest) {
-  const supabase = createClient()
-  const { data: { session } } = await supabase.auth.getSession()
+  let supabaseResponse = NextResponse.next({
+    request,
+  });
 
-  // Allow public access to the landing page
-  if (request.nextUrl.pathname === '/') {
-    return NextResponse.next()
-  }
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll();
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) =>
+            request.cookies.set(name, value)
+          );
+          supabaseResponse = NextResponse.next({
+            request,
+          });
+          cookiesToSet.forEach(({ name, value, options }) =>
+            supabaseResponse.cookies.set(name, value, options)
+          );
+        },
+      },
+    }
+  );
 
-  // Redirect to /login if the user is not authenticated
-  if (!session) {
-    return NextResponse.redirect(new URL('/login', request.url))
-  }
+  // refreshing the auth token
+  await supabase.auth.getUser();
 
-  return NextResponse.next()
+  return supabaseResponse;
 }
